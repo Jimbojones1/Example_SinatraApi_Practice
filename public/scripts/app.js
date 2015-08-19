c2287b0d406752e62cf4e79def48b6748a1fdf87
James Haff <jehaff@James-Haffs-MBP.attlocal.net>
2015-08-19 00:34:22 -0500

    finished

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,4 +20,18 @@ $(document).ready(function(){
     }
   });
 
+  $('body').css('background-color', 'rgba(84, 123, 213, 0.61)');
+  $('.table').css('margin-top', '10%');
+  $('thead').css('background-color', 'rgba(138, 36, 200, 0.36');
+  $('.navbar').css('height', '130px').css('background', 'repeating-radial-gradient(black, rgba(138, 36, 200, 0.36) 15px, white 20px,rgba(138, 36, 200, 0.36) 20px').css('color', 'rgba(213, 211, 18, 0.72)');
+  $('h1').css('text-align', 'center').css('color', 'rgba(66, 11, 11, 0.84)').css('font-weight', 'bolder');
+  $('h1').load(function(){
+    $(this).fadeIn('2000')});
+  $('img').load(function(){
+    $(this).animate({
+      marginLeft: "6.0in"
+    },2000)
+  });
+  $('p').css('marginLeft', '10px').css('color', 'white');
+
 });//document ready
